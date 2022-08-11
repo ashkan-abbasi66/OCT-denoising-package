419e4be18034ec2b4703134a2f28ad39bd02789d
Ashkan Abbasi <ashkan.abbasi66@yahoo.com>
2022-08-11 10:58:43 -0700

    Delete Methods directory

--- Methods/TensorDL/normalized.m
+++
@@ -1,5 +0,0 @@
-function Y = normalized(X)
-maxX = max(X(:));
-minX = min(X(:));
-Y    = (X-minX)/(maxX-minX);
-end
--- Methods/utils/take_ilog.m
+++
@@ -1,6 +0,0 @@
-function img_out = take_ilog(img_log)
-% Take inverse logarithm transformation
-% 
-% See "take_log"
-    img_out = exp(img_log) - 1;
-end
